15c00ab6170bde710d338f9533016a917d670cbb
Popov Adrian <qwasuss@gmail.com>
2024-04-29 14:16:26 +0300

    Deploy an nginx server on DigitalOcean

---
+++ www-1.tf
@@ -0,0 +1,27 @@
+resource "digitalocean_droplet" "www-1" {
+  image = "ubuntu-22-04-x64"
+  name = "something"
+  region = "fra1"
+  size = "s-2vcpu-2gb"
+  ssh_keys = [
+    data.digitalocean_ssh_key.terraform.id
+  ]
+  
+  connection {
+    host = self.ipv4_address
+    user = "root"
+    type = "ssh"
+    private_key = file(var.pvt_key)
+    timeout = "2m"
+  }
+  
+  provisioner "remote-exec" {
+    inline = [
+      "export PATH=$PATH:/usr/bin",
+      # install nginx
+      "sudo apt update",
+      "sudo apt install -y nginx",
+      "sleep 1"
+    ]
+  }
+}
